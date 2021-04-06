8247c9e16dd7bf19d60afbeabbe1f72a4c77d7d2
Joshua <jssmiller94@gmail.com>
2021-04-06 14:01:04 +0100

    great success

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -3,6 +3,7 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = 0
 
     def drive(self):
         return "Brum brum"
